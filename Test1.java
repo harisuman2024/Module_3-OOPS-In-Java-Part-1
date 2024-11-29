774e72df7b507680f7079138d59887d8d72fc666
HARI SUMAN <95356493+harisuman2024@users.noreply.github.com>
2024-11-29 23:15:01 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,9 @@
+class Test1{
+public static void main(String args[]){
+Student st=new Student();
+System.out.println(st.sname);
+} 
+}
+class Student{
+String sname;
+} 
---
+++ Test7.java
@@ -0,0 +1,9 @@
+class Test7{
+public static void main(String args[]){
+System.out.println(new Student().name="Sri");
+System.out.println(new Student().name);
+}
+}
+class Student{
+static String name;
+}
